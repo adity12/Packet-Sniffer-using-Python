60b509d5edf9a818e997c50da48ae600b17585d3
adity12 <68803969+adity12@users.noreply.github.com>
2024-01-23 11:42:33 +0000

    Scapy-Sniffer

--- Scapy-Sniffer.py
+++ Scapy-Sniffer.py
@@ -1,1 +1,13 @@
-import scapy.all
+from scapy.all import *
+from scapy.layers.inet import IP
+from scapy.layers.http import HTTPRequest, TCP
+from colorama import init, fore
+
+
+init()
+
+red = fore.RED
+green = fore.GREEN
+blue = fore.BLUE
+reset = fore.RESET
+
